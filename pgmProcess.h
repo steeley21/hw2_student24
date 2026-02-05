028cf50a07ddda4dc67f4a324ebebfee9e777444
Ethan Guerin <129787373+EthanAGi@users.noreply.github.com>
2026-02-04 19:37:32 -0800

    Fix: Added guards to Process, and fixed mismatched headers.
    Added guards to pgmProcess.h, and fixed mismatched headers.

--- pgmProcess.h
+++ pgmProcess.h
@@ -1,3 +1,6 @@
+#ifndef PGM_PROCESS_H
+#define PGM_PROCESS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -5,16 +8,27 @@
 /**
  *  Function Name:
  *      distance()
- *      distance() returns the Euclidean distance between two pixels. This function is executed on CUDA device
+ *      distance() returns the Euclidean distance between two pixels.
+ *      This function is executed on CUDA device.
  *
  *  @param[in]  p1  coordinates of pixel one, p1[0] is for row number, p1[1] is for column number
  *  @param[in]  p2  coordinates of pixel two, p2[0] is for row number, p2[1] is for column number
  *  @return         return distance between p1 and p2
  */
-__device__ float distance( int p1[], int p2[] );
 
-__global__ void drawCircleKernel(int *pixels, int numRows, int numCols, int centerRow, int centerCol, int radius);
+#ifdef __CUDACC__
 
-__global__ void drawEdgeKernel(int *pixels, int numRows, int numCols, int edgeWidth);
+__device__ float distance(int p1[], int p2[]);
 
-__global__ void drawLineKernel(int *pixels, int numRows, int numCols, int p1row, int p1col, int p2row, int p2col);
+__global__ void drawCircleKernel(int *pixels, int numRows, int numCols,
+                                 int centerRow, int centerCol, int radius);
+
+__global__ void drawEdgeKernel(int *pixels, int numRows, int numCols,
+                               int edgeWidth);
+
+__global__ void drawLineKernel(int *pixels, int numRows, int numCols,
+                               int p1row, int p1col, int p2row, int p2col);
+
+#endif  // __CUDACC__
+
+#endif  // PGM_PROCESS_H
--- seqPgmUtility.h
+++ seqPgmUtility.h
@@ -8,20 +8,25 @@
 #ifndef cscd439pgm_seqPgmUtility_h
 #define cscd439pgm_seqPgmUtility_h
 
+#include <stdio.h>
 #include <math.h>
 #include "pgmProcess.h"
 
 #define rowsInHeader 4      // number of rows in image header
-#define maxSizeHeadRow 200  // maximal number characters in one row in the header
+#define maxSizeHeadRow 200  // maximal number characters in one row in header
 
-int * seqPgmRead( char **header, int *numRows, int *numCols, FILE *in  );
+int ** seqPgmRead(char **header, int *numRows, int *numCols, FILE *in);
 
-int seqPgmDrawCircle( int *pixels, int numRows, int numCols, int centerRow, int centerCol, int radius, char **header );
+int seqPgmDrawCircle(int **pixels, int numRows, int numCols,
+                     int centerRow, int centerCol, int radius, char **header);
 
-int seqPgmDrawEdge( int *pixels, int numRows, int numCols, int edgeWidth, char **header );
+int seqPgmDrawEdge(int **pixels, int numRows, int numCols,
+                   int edgeWidth, char **header);
 
-int seqPgmDrawLine( int *pixels, int numRows, int numCols, char **header, int p1row, int p1col, int p2row, int p2col );
+int seqPgmDrawLine(int **pixels, int numRows, int numCols, char **header,
+                   int p1row, int p1col, int p2row, int p2col);
 
-int seqPgmWrite( const char **header, const int *pixels, int numRows, int numCols, FILE *out );
+int seqPgmWrite(const char **header, const int **pixels,
+                int numRows, int numCols, FILE *out);
 
 #endif
